35a5119e9e16f028fa55ff732e40f9adcfbe212f
luanmvcosta0 <luanmvcosta@mgail.com>
2025-03-10 23:36:24 -0300

    Criacao do CRUD - UPDATE, criando o useCase

---
+++ src/main/java/com/costa/hexagonal/application/core/usecase/UpdateCustomerUseCase.java
@@ -0,0 +1,31 @@
+package com.costa.hexagonal.application.core.usecase;
+
+import com.costa.hexagonal.application.core.domain.Customer;
+import com.costa.hexagonal.application.ports.in.FindCustomerByIdInputPort;
+import com.costa.hexagonal.application.ports.out.FindAddressByZipCodeOutPutPort;
+import com.costa.hexagonal.application.ports.out.UpdateCustomerOutPutPort;
+
+public class UpdateCustomerUseCase {
+
+    private final FindCustomerByIdInputPort findCustomerByIdInputPort;
+    private final FindAddressByZipCodeOutPutPort findAddressByZipCodeOutPutPort;
+    private final UpdateCustomerOutPutPort updateCustomerOutPutPort;
+
+    public UpdateCustomerUseCase(
+            FindCustomerByIdInputPort findCustomerByIdInputPort,
+            FindAddressByZipCodeOutPutPort findAddressByZipCodeOutPutPort,
+            UpdateCustomerOutPutPort updateCustomerOutPutPort)
+    {
+        this.findCustomerByIdInputPort = findCustomerByIdInputPort;
+        this.findAddressByZipCodeOutPutPort = findAddressByZipCodeOutPutPort;
+        this.updateCustomerOutPutPort = updateCustomerOutPutPort;
+    }
+
+    public void update(Customer customer, String zipCode) {
+        findCustomerByIdInputPort.find(customer.getId());
+        var address = findAddressByZipCodeOutPutPort.find(zipCode);
+        customer.setAddress(address);
+        updateCustomerOutPutPort.update(customer);
+    }
+
+}
